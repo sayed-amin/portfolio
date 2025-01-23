3ddb6b6238e2b7ab274492b491e3ef8c7f0bfea9
amin <92389792+sayed-amin@users.noreply.github.com>
2025-01-24 01:03:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(() => {
     return {
         build: {
+             base: './', 
             outDir: 'dist',
             chunkSizeWarningLimit: 1000, // Increase the chunk size warning limit (default is 500kB)
             rollupOptions: {
